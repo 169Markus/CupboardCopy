f79a58ec229f4cc81880aa9a14e5808946c26b5d
Luke Miles <luke.49@live.co.uk>
2018-11-24 19:59:58 +0000

    Fix problems

--- recipeFinder.js
+++ recipeFinder.js
@@ -1,34 +1,48 @@
 
 // Setup code
 let ingredients;
+let ids;
 let recipes;
+
+let a = 0, b = 0;
 
 async function init() {
 	ingredients = await fetch("./ingredients.json").then(r => r.json());
+	ids = Object.keys(ingredients);
 	recipes = await fetch("./recipes.json").then(r => r.json());
+}
+
+function findID(id) {
+	if (ingredients[id]) return ingredients[id];
+	
+	let from = 0, to = ids.length - 1;
+	
+	while (from + 1 != to) {
+		let middle = Math.floor((from + to) / 2);
+		
+		if (ids[middle] > id) to = middle;
+		else from = middle;
+	}
+	
+	if (ids[to] - id > id - ids[from]) return ingredients[ids[from]];
+	else return ingredients[ids[to]];
 }
 
 // Function
 // cupboard = cupboard = ["Pork", "Chicken", "Pasta", "Pasta Sauce"]
 function findRecipes(cupboard) {
-	let recipefound = new Array (501);
-	for (let element in recipefound){
-		recipefound[element] = [];
-	}
 	for (let recipe of recipes){
-		let wrong = 0;
 		recipe.nothave = [];
 		for (let ingredient of recipe.ingredients){
-			if (! cupboard.includes(ingredients[ingredient.sku])){
-				wrong++;
-				recipe.nothave.push(ingredients[ingredient.sku]);
-			}
+			let name = findID(ingredient.sku);
 			
+			if (!cupboard.includes(name)){
+				recipe.nothave.push(name);
+			}
 		}
-		recipefound[wrong].push(recipe);
 	}
 
-	return recipefound.flat();
+	return recipes.sort((a, b) => a.nothave.length / a.ingredients.length - b.nothave.length / b.ingredients.length);
 }
 
 init();
